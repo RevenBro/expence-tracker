790ddd812e23b6a541cfdc0497caeb354bfea4a1
RevenBro <saidakbarov077@gmail.com>
2025-03-04 21:27:44 +0500

    Styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Project started</h1>
+    <div className='flex flex-col items-center mt-[30px] mb-[10px] font-[Montserrat]'>
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-black text-[25px] font-bold">Expence Tracker</h1>
+    </div>
+  )
+}
+
+export default Header
