7b418283b0175cde9a31d18bdde365c7703e3248
RevenBro <saidakbarov077@gmail.com>
2025-03-08 23:23:02 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home'
 function App() {
 
   return (
-    <div className='flex flex-col items-center text-center mt-[30px] mb-[10px] font-[Montserrat]'>
+    <div className='flex flex-col items-center text-center mt-[25px] mb-[10px] font-[Montserrat]'>
       <Home/>
     </div>
   )
